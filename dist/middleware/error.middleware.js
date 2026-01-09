8309579680d5d766437fe70b2b6c0415f000d264
Subham Kulu <subhamkulu06@gmail.com>
2026-01-10 00:13:18 +0530

    fix: add missing types for render build

--- dist/middleware/error.middleware.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorHandler = void 0;
-const errorHandler = (err, _req, res, _next) => {
-    res.status(500).json({ message: err.message || "Server error" });
-};
-exports.errorHandler = errorHandler;
